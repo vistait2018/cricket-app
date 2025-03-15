2e9a56b05badf15261c9c103976eb1926fb67b60
Java20091@ <jidedorcas@gmail.com>
2025-03-15 12:15:59 +0100

    APi DOumentations

--- src/main/java/com/pks/cricket_app/controller/PlayerController.java
+++ src/main/java/com/pks/cricket_app/controller/PlayerController.java
@@ -4,6 +4,7 @@ package com.pks.cricket_app.controller;
 import com.pks.cricket_app.dtos.PlayerRequest;
 import com.pks.cricket_app.dtos.PlayerResponse;
 import com.pks.cricket_app.services.PlayerService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,8 @@ public class PlayerController {
     private static final Logger log = LoggerFactory.getLogger(PlayerController.class);
 
     @PostMapping
-    public ResponseEntity<PlayerResponse> addPlayer(@RequestBody PlayerRequest playerRequest) {
+    public ResponseEntity<PlayerResponse> addPlayer(
+            @Valid @RequestBody PlayerRequest playerRequest) {
         log.info("Starting in the add player controller");
 
         PlayerResponse playerResponse =
--- src/main/java/com/pks/cricket_app/dtos/PlayerRequest.java
+++ src/main/java/com/pks/cricket_app/dtos/PlayerRequest.java
@@ -1,11 +1,19 @@
 package com.pks.cricket_app.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 public class PlayerRequest {
+
+    @NotNull
+    @NotEmpty
     private  String name;
+
+    @NotNull
+    @NotEmpty
     private String role;
 
 
